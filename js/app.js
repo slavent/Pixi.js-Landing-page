31e6e923f9f6a27d619edcfd1c53c70db77e8be5
Slavent <startupne@gmail.com>
2015-05-13 13:11:58 +0300

    refactoring

--- js/app.js
+++ js/app.js
@@ -7,22 +7,7 @@
 		w_width = $(window).width(),
 		w_height = $(window).height(),
 		renderer = new PIXI.autoDetectRenderer(w_width, w_height, { transparent: true }),
-		stage = new PIXI.Container(),
-
-		// Slide 1
-		s1_logo,
-		s1_logo_texture,
-		s1_title,
-		s1_texture,
-		s1_videoSprite,
-		s1_dot1_texture,
-		s1_dot2_texture,
-		s1_dot3_texture,
-		s1_dot1,
-		s1_dot2,
-		s1_dot3,
-		s1_count = 0;
-
+		stage = new PIXI.Container();
     // END: Pixi basic
 
 
@@ -60,18 +45,31 @@
 
 		    function animate() {
 		    	// Slide 1
-	    		s1_count += 0.06;
-	    		spin2.y += Math.sin(s1_count);
+		    	App.manager.slide_1.spinner.update();
 
 		        renderer.render(stage);
 		        requestAnimationFrame(animate);
 		    }			
 
 		  	App.bindEvents(1);
-		  	App.manager.slide_1();
+		  	App.manager.slide_1.init();
 		},
 
 		bindEvents: function(slide_id) {
+			var $menu_icon = $(".menu-icon-wrp"),
+				$menu_popup = $(".menu-popup"),
+				$menu_close_btn = $(".menu-popup-closebtn");
+
+			$menu_icon.on("click", function() {
+    			$menu_popup.fadeIn();
+    			$(this).fadeOut();
+    		});
+
+    		$menu_close_btn.on("click", function() {
+    			$menu_icon.fadeIn();
+    			$menu_popup.hide();
+    		});
+
 
     		switch(slide_id) {
     			case 1: 
@@ -101,117 +99,165 @@
     				break;
     		}	
 
-    		$(".menu-icon-wrp").on("click", function() {
-    			$(".menu-popup, .menu-popup-closebtn").fadeIn();
-    			$(this).fadeOut();
-    		});
-
-    		$(".menu-popup-closebtn").on("click", function() {
-    			$(".menu-icon-wrp").fadeIn();
-    			$(this).fadeOut();
-    			$(".menu-popup").fadeOut();
-    		});
-
 	    },
 
 		manager: {
 
-			slide_1: function() {
-				console.log("Slide 1 start");
+			slide_1: {
 
-				// Video
-				s1_texture = PIXI.Texture.fromVideo("http://alexapokrashenko.com/wp-content/themes/alexa/video.mp4");
-				s1_videoSprite = new PIXI.Sprite(s1_texture);
-				s1_videoSprite.width = renderer.width;
-				s1_videoSprite.height = renderer.height;
-				stage.addChild(s1_videoSprite, function() {
-					App.bindEvents(2);
-				});
+				init: function() {
+					this.video.init();
+					this.logo.init();
+					this.title.init();
+					this.slider.init();
+					this.spinner.init();
+				},
 
-				// Logo
-				s1_logo_texture = PIXI.Texture.fromImage("i/s1/logo.svg");
-				s1_logo = new PIXI.Sprite(s1_logo_texture);
-				s1_logo.anchor.set(0.5);
-				s1_logo.position.x = renderer.width / 2;
-				s1_logo.position.y = renderer.height / 2;
-				stage.addChild(s1_logo);
+				video: {
+					init: function() {
+						var texture = PIXI.Texture.fromVideo("video.mp4"),
+							videoSprite = new PIXI.Sprite(texture);
 
-				// Title
-				var style = {
-					font : '38px HelveticaNeueCyr-Light',
-				    fill : '#fff',
-				    align : "center",
-				    lineHeight : 50,
-				    padding : 50
-				};
-				s1_title = new PIXI.Text("ДОБРО ПОЖАЛОВАТЬ В ЛАБОРАТОРИЮ\nСТИЛЯ «МОДНОГО ПРИГОВОРА»!", style);
-				s1_title.x = (renderer.width - s1_title.width) / 2;	
-				s1_title.y = ((renderer.height - s1_title.height) / 2) + 180;
-				stage.addChild(s1_title);
+						videoSprite.width = renderer.width;
+						videoSprite.height = renderer.height;
 
-				// Slider nav
-				s1_dot1_texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-				s1_dot1 = new PIXI.Sprite(s1_dot1_texture);
-				s1_dot1.anchor.set(0.5);
-				s1_dot1.position.x = renderer.width / 2 - 25;
-				s1_dot1.position.y = renderer.height / 2 + 200;
-				s1_dot1.buttonMode = true;
-				s1_dot1.interactive = true;
-				s1_dot1.on("click", function() {
-					this.texture = PIXI.Texture.fromImage("i/s1/dot_active.svg");
-					s1_dot2.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-					s1_dot3.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-				});
-				stage.addChild(s1_dot1);
+						stage.addChild(videoSprite, function() {
+							App.bindEvents(2);
+						});
+					}
+				},
 
-				s1_dot2_texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-				s1_dot2 = new PIXI.Sprite(s1_dot2_texture);
-				s1_dot2.anchor.set(0.5);
-				s1_dot2.position.x = renderer.width / 2 - 5;
-				s1_dot2.position.y = renderer.height / 2 + 200;
-				s1_dot2.buttonMode = true;
-				s1_dot2.interactive = true;
-				s1_dot2.on("click", function() {
-					this.texture = PIXI.Texture.fromImage("i/s1/dot_active.svg");
-					s1_dot1.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-					s1_dot3.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-				});
-				stage.addChild(s1_dot2);
+				logo: {
+					init: function() {
+						var logo_texture = PIXI.Texture.fromImage("i/s1/logo.svg"),
+							logo = new PIXI.Sprite(logo_texture);
 
-				s1_dot3_texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-				s1_dot3 = new PIXI.Sprite(s1_dot3_texture);
-				s1_dot3.anchor.set(0.5);
-				s1_dot3.position.x = renderer.width / 2 + 15;
-				s1_dot3.position.y = renderer.height / 2 + 200;
-				s1_dot3.buttonMode = true;
-				s1_dot3.interactive = true;
-				s1_dot3.on("click", function() {
-					this.texture = PIXI.Texture.fromImage("i/s1/dot_active.svg");
-					s1_dot1.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-					s1_dot2.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
-				});
-				stage.addChild(s1_dot3);
+						logo.anchor.set(0.5);
+						logo.position.x = renderer.width / 2;
+						logo.position.y = renderer.height / 2;
 
-				// Spinner
-				spin1_texture = PIXI.Texture.fromImage("i/s1/spin1.svg");
-				spin1 = new PIXI.Sprite(spin1_texture);
-				spin1.anchor.set(0.5);
-				spin1.position.x = renderer.width / 2 - 5;
-				spin1.position.y = renderer.height / 2 + 350;
-				spin1.buttonMode = true;
-				spin1.interactive = true;
-				stage.addChild(spin1);
+						stage.addChild(logo);
+					}
+				},
 
-				spin2_texture = PIXI.Texture.fromImage("i/s1/spin2.svg");
-				spin2 = new PIXI.Sprite(spin2_texture);
-				spin2.anchor.set(0.5);
-				spin2.position.x = renderer.width / 2 - 5;
-				spin2.position.y = renderer.height / 2 + 345;
-				spin2.buttonMode = true;
-				spin2.interactive = true;
-				stage.addChild(spin2);
+				title: {
+					init: function() {
+						var style = {
+								font : '38px HelveticaNeueCyr-Light',
+							    fill : '#fff',
+							    align : "center",
+							    lineHeight : 50,
+							    padding : 50
+							},
+							title = new PIXI.Text("ДОБРО ПОЖАЛОВАТЬ В ЛАБОРАТОРИЮ\nСТИЛЯ «МОДНОГО ПРИГОВОРА»!", style);
 
-				console.log();
+						title.x = (renderer.width - title.width) / 2;	
+						title.y = ((renderer.height - title.height) / 2) + 180;
+
+						stage.addChild(title);
+					}
+				},
+
+				slider: {
+
+					init: function() {
+						this.makeNav();
+					},
+
+					makeNav: function() {
+						var dot1_texture = PIXI.Texture.fromImage("i/s1/dot_active.svg"),
+							dot1 = new PIXI.Sprite(dot1_texture),
+							dot2_texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+							dot2 = new PIXI.Sprite(dot2_texture),
+							dot3_texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+							dot3 = new PIXI.Sprite(dot3_texture);
+
+						dot1.anchor.set(0.5);
+						dot1.position.x = renderer.width / 2 - 25;
+						dot1.position.y = renderer.height / 2 + 200;
+						dot1.buttonMode = true;
+						dot1.interactive = true;
+
+						dot1.on("click", function() {
+							this.texture = PIXI.Texture.fromImage("i/s1/dot_active.svg");
+							dot2.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+							dot3.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+						});
+
+						
+
+						dot2.anchor.set(0.5);
+						dot2.position.x = renderer.width / 2 - 5;
+						dot2.position.y = renderer.height / 2 + 200;
+						dot2.buttonMode = true;
+						dot2.interactive = true;
+
+						dot2.on("click", function() {
+							this.texture = PIXI.Texture.fromImage("i/s1/dot_active.svg");
+							dot1.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+							dot3.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+						});
+
+						
+
+						dot3.anchor.set(0.5);
+						dot3.position.x = renderer.width / 2 + 15;
+						dot3.position.y = renderer.height / 2 + 200;
+						dot3.buttonMode = true;
+						dot3.interactive = true;
+
+						dot3.on("click", function() {
+							this.texture = PIXI.Texture.fromImage("i/s1/dot_active.svg");
+							dot1.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+							dot2.texture = PIXI.Texture.fromImage("i/s1/dot.svg");
+						});
+
+						stage.addChild(dot1);
+						stage.addChild(dot2);
+						stage.addChild(dot3);
+					}	
+
+				},
+
+				spinner: {
+					vars: {
+						spin1_texture: 	null,
+						spin1: 			null,
+						spin2_texture: 	null,
+						spin2: 			null,
+						count: 			0
+					},
+
+					init: function() {
+						var vars = this.vars;
+
+						vars.spin1_texture = 	PIXI.Texture.fromImage("i/s1/spin1.svg"),
+						vars.spin1 = 			new PIXI.Sprite(vars.spin1_texture),
+						vars.spin2_texture = 	PIXI.Texture.fromImage("i/s1/spin2.svg"),
+						vars.spin2 = 			new PIXI.Sprite(vars.spin2_texture);
+
+						vars.spin1.anchor.set(0.5);
+						vars.spin1.position.x = renderer.width / 2 - 5;
+						vars.spin1.position.y = renderer.height / 2 + 350;
+						vars.spin1.buttonMode = true;
+						vars.spin1.interactive = true;
+
+						vars.spin2.anchor.set(0.5);
+						vars.spin2.position.x = renderer.width / 2 - 5;
+						vars.spin2.position.y = renderer.height / 2 + 345;
+						vars.spin2.buttonMode = true;
+						vars.spin2.interactive = true;
+
+						stage.addChild(vars.spin1);
+						stage.addChild(vars.spin2); 
+					},
+
+					update: function() {
+						this.vars.count += 0.06;
+	    				this.vars.spin2.y += Math.sin(this.vars.count);
+					}
+
+				}
 			},
 
 			slide_2: function() {
