e8e2fb660fdb62994919ced2181d109399a9c73a
Slavent <startupne@gmail.com>
2015-06-11 16:17:01 +0300

    slide 3 to json

--- js/Slide_3.js
+++ js/Slide_3.js
@@ -208,159 +208,94 @@ App.managerService.slide_3 = {
 				$hyde_menu.removeClass("active");
 			},
 
-			Scene: function(url, title_top, title_down) {
+			Scene: function(data) {
 				// Titles
-				if( mobile_version == true ) {
-					var style = {
-						font : "35px PlumbCondensed-Bold",
-					    fill : "#000000",
-					    align : "center"
-					},
-					title_top = new PIXI.Text(title_top, style),
-					title_down = new PIXI.Text(title_down, style);
+				var style = {
+					font 	: data.titles.font,
+				    fill 	: data.titles.fill,
+				    align 	: data.titles.align
+				},
+				title_top = new PIXI.Text(data.titles.title_top.text, style),
+				title_down = new PIXI.Text(data.titles.title_down.text, style);
 
-					title_top.x = -1000;
-					title_top.y = -300;
-					title_top.anchor.set(0.5);
+				title_top.x = data.titles.title_top.x;
+				title_top.y = data.titles.title_top.y;
+				title_top.anchor.set(data.titles.anchor);
 
-					title_down.x = 1000;
-					title_down.y = 300;
-					title_down.anchor.set(0.5);
-				} else {
-					var style = {
-						font : "22px PlumbCondensed-Bold",
-					    fill : "#000000",
-					    align : "center"
-					},
-					title_top = new PIXI.Text(title_top, style),
-					title_down = new PIXI.Text(title_down, style);
-
-					title_top.x = -1000;
-					title_top.y = -50;
-
-					title_down.x = 1000;
-					title_down.y = 450;
-				}
+				title_down.x = data.titles.title_down.x;
+				title_down.y = data.titles.title_down.y;
+				title_down.anchor.set(data.titles.anchor);
 
 				// Title's lines
-				if( mobile_version == true ) {
-					var data_lines = [
-						{ color: "0x000000", size: title_top.width - 100, 	x: -15, 						y: 50, pos: "top" },
-						{ color: "0x000000", size: title_top.width - 100, 	x: title_top.height + 15, 		y: 50, pos: "top" },
-						{ color: "0x000000", size: title_down.width - 100, 	x: -15, 						y: 50, pos: "down" },
-						{ color: "0x000000", size: title_down.width - 100, 	x: title_down.height + 15, 		y: 50, pos: "down" },
-					];
+				var data_lines = [
+					{ color: "0x000000", size: title_top.width - 100, 	x: -15, 						y: 50, pos: "top" },
+					{ color: "0x000000", size: title_top.width - 100, 	x: title_top.height + 15, 		y: 50, pos: "top" },
+					{ color: "0x000000", size: title_down.width - 100, 	x: -15, 						y: 50, pos: "down" },
+					{ color: "0x000000", size: title_down.width - 100, 	x: title_down.height + 15, 		y: 50, pos: "down" },
+				];
 
-					var Line = function(color, size, x, y) {
-						this.el = new PIXI.Graphics();
-						this.el.beginFill(0xffffff);
-						this.el.lineStyle(3, color, 1);
-						this.el.moveTo(0,0);
-						this.el.lineTo(size,0);
-						this.el.endFill();
-						this.el.position.x = y;
-						this.el.position.y = x;
+				var Line = function(color, size, x, y) {
+					this.el = new PIXI.Graphics();
+					this.el.beginFill(0xffffff);
+					this.el.lineStyle(1, color, 1);
+					this.el.moveTo(0,0);
+					this.el.lineTo(size,0);
+					this.el.endFill();
+					this.el.position.x = y;
+					this.el.position.y = x;
 
-						return this.el;
-					};
+					return this.el;
+				};
 
-					/*for(var i = 0; i < data_lines.length; i++) {
-						var line = new Line( data_lines[i].color, data_lines[i].size, data_lines[i].x, data_lines[i].y );
+				for(var i = 0; i < data_lines.length; i++) {
+					var line = new Line( data_lines[i].color, data_lines[i].size, data_lines[i].x, data_lines[i].y );
 
-						switch(data_lines[i].pos) {
-							case "top":
-								title_top.addChild(line);
-								break;
-							case "down":
-								title_down.addChild(line);
-								break;
-						}
-					}*/
-				} else {
-					var data_lines = [
-						{ color: "0x000000", size: title_top.width - 100, 	x: -15, 						y: 50, pos: "top" },
-						{ color: "0x000000", size: title_top.width - 100, 	x: title_top.height + 15, 		y: 50, pos: "top" },
-						{ color: "0x000000", size: title_down.width - 100, 	x: -15, 						y: 50, pos: "down" },
-						{ color: "0x000000", size: title_down.width - 100, 	x: title_down.height + 15, 		y: 50, pos: "down" },
-					];
-
-					var Line = function(color, size, x, y) {
-						this.el = new PIXI.Graphics();
-						this.el.beginFill(0xffffff);
-						this.el.lineStyle(1, color, 1);
-						this.el.moveTo(0,0);
-						this.el.lineTo(size,0);
-						this.el.endFill();
-						this.el.position.x = y;
-						this.el.position.y = x;
-
-						return this.el;
-					};
-
-					for(var i = 0; i < data_lines.length; i++) {
-						var line = new Line( data_lines[i].color, data_lines[i].size, data_lines[i].x, data_lines[i].y );
-
-						switch(data_lines[i].pos) {
-							case "top":
-								title_top.addChild(line);
-								break;
-							case "down":
-								title_down.addChild(line);
-								break;
-						}
+					switch(data_lines[i].pos) {
+						case "top":
+							title_top.addChild(line);
+							break;
+						case "down":
+							title_down.addChild(line);
+							break;
 					}
 				}
 
 				// Photo
-				if( mobile_version == true ) {
-					this.texture = PIXI.Texture.fromImage(url);
-					this.el = new PIXI.Sprite(this.texture);
-					this.el.scale.set(1.2);
-					this.el.anchor.set(0.5);
-					this.el.position.x = renderer.width / 2;
-					this.el.position.y = renderer.height * 2;
-					this.el.addChild(title_top);
-					this.el.addChild(title_down);
-				} else {
-					this.texture = PIXI.Texture.fromImage(url);
-					this.el = new PIXI.Sprite(this.texture);
-					this.el.position.x = renderer.width / 2;
-					this.el.position.y = renderer.height * 2;
-					this.el.addChild(title_top);
-					this.el.addChild(title_down);
-				}
+				this.texture = PIXI.Texture.fromImage(data.photo.url);
+				this.el = new PIXI.Sprite(this.texture);
+				this.el.scale.set(data.photo.scale);
+				this.el.anchor.set(data.photo.anchor);
+				this.el.position.x = renderer.width / 2;
+				this.el.position.y = renderer.height * 2;
+				this.el.addChild(title_top);
+				this.el.addChild(title_down);
 
 				this.el.init = function(element) {
-					if( mobile_version == true ) {
-						createjs.Tween.get(element)
-							.to({ y: renderer.height / 2 + 250 }, 1000, createjs.Ease.getPowInOut(4));
-						createjs.Tween.get(title_top)
-							.wait(500)
-							.to({ x: 0 }, 1000, createjs.Ease.getPowInOut(4));
-						createjs.Tween.get(title_down)
-							.wait(500)
-							.to({ x: 0 }, 1000, createjs.Ease.getPowInOut(4));
-					} else {
-						createjs.Tween.get(element)
-							.to({ y: renderer.height / 2 - 250 }, 1000, createjs.Ease.getPowInOut(4));
-						createjs.Tween.get(title_top)
-							.wait(500)
-							.to({ x: -50 }, 1000, createjs.Ease.getPowInOut(4));
-						createjs.Tween.get(title_down)
-							.wait(500)
-							.to({ x: 350 }, 1000, createjs.Ease.getPowInOut(4));
-					}
+					createjs.Tween.get(element)
+						.to({ y: renderer.height / 2 + data.photo.y }, 1000, createjs.Ease.getPowInOut(4));
+
+					createjs.Tween.get(title_top)
+						.wait(500)
+						.to({ x: data.titles.title_top.x2 }, 1000, createjs.Ease.getPowInOut(4));
+
+					createjs.Tween.get(title_down)
+						.wait(500)
+						.to({ x: data.titles.title_down.x2 }, 1000, createjs.Ease.getPowInOut(4));
+
+					App.managerService.slide_3.elems.menu["order_btn"].init();
 				}
 
 				this.el.destroy = function(element) {
-					createjs.Tween.get(title_top).to({ x: -1000 }, 500, createjs.Ease.getPowInOut(4));
-					createjs.Tween.get(title_down).to({ x: 1000 }, 500, createjs.Ease.getPowInOut(4));
+					createjs.Tween.get(title_top).to({ x: data.titles.title_top.x }, 500, createjs.Ease.getPowInOut(4));
+					createjs.Tween.get(title_down).to({ x: data.titles.title_down.x }, 500, createjs.Ease.getPowInOut(4));
 					createjs.Tween.get(element)
 						.wait(200)
 						.to({ y: -renderer.height * 2 }, 1000, createjs.Ease.getPowInOut(4))
 							.call(function() {
 								slide_container_3.removeChild(this.el);
 							});
+
+					App.managerService.slide_3.elems.menu["order_btn"].destroy();
 				}
 
 				return this.el;
@@ -371,14 +306,12 @@ App.managerService.slide_3 = {
 				el: null,
 
 				init: function() {
-					var deferred = $.Deferred(),
-						title_top = "ХОТИТЕ ЗНАТЬ, КАКИЕ ЦВЕТА ПОДХОДЯТ ВАМ ИДЕАЛЬНО?\nДЛЯ ВАШЕГО ИМИДЖ-ГАЙДА СПЕЦИАЛИСТ СОСТАВИТ\nПЕРСОНАЛЬНУЮ ЦВЕТОВУЮ КАРТУ ВАШЕЙ ВНЕШНОСТИ!",
-						title_down = "ТЕПЕРЬ ВСЯ ОДЕЖДА БУДЕТ ПОДЧЕРКИВАТЬ КРАСОТУ\nВАШИХ ГЛАЗ И НЕЖНЫЙ ОТТЕНОК ВАШЕЙ КОЖИ.";
-
 					App.managerService.slide_3.elems.menu.init();
-					this.el = new App.managerService.slide_3.elems.menu.Scene("i/s3/1/wears.jpg", title_top, title_down);
+					this.el = new App.managerService.slide_3.elems.menu.Scene(DATA.slide_3.scene_1);
 					slide_container_3.addChild(this.el);
 					this.el.init(this.el);
+
+					var deferred = $.Deferred();
 
 					setTimeout(function() {
 						deferred.resolve();
@@ -398,13 +331,11 @@ App.managerService.slide_3 = {
 				el: null,
 
 				init: function() {
-					var deferred = $.Deferred(),
-						title_top = "ХОТИТЕ ЗНАТЬ, КАКИЕ ЦВЕТА ПОДХОДЯТ ВАМ ИДЕАЛЬНО?\nДЛЯ ВАШЕГО ИМИДЖ-ГАЙДА СПЕЦИАЛИСТ СОСТАВИТ\nПЕРСОНАЛЬНУЮ ЦВЕТОВУЮ КАРТУ ВАШЕЙ ВНЕШНОСТИ!",
-						title_down = "ТЕПЕРЬ ВСЯ ОДЕЖДА БУДЕТ ПОДЧЕРКИВАТЬ КРАСОТУ\nВАШИХ ГЛАЗ И НЕЖНЫЙ ОТТЕНОК ВАШЕЙ КОЖИ.";
-
-					this.el = new App.managerService.slide_3.elems.menu.Scene("i/s3/2/wears.jpg", title_top, title_down);
+					this.el = new App.managerService.slide_3.elems.menu.Scene(DATA.slide_3.scene_2);
 					slide_container_3.addChild(this.el);
 					this.el.init(this.el);
+
+					var deferred = $.Deferred();
 
 					setTimeout(function() {
 						deferred.resolve();
@@ -425,13 +356,11 @@ App.managerService.slide_3 = {
 				el: null,
 
 				init: function() {
-					var deferred = $.Deferred(),
-						title_top = "ХОТИТЕ ЗНАТЬ, КАКИЕ ЦВЕТА ПОДХОДЯТ ВАМ ИДЕАЛЬНО?\nДЛЯ ВАШЕГО ИМИДЖ-ГАЙДА СПЕЦИАЛИСТ СОСТАВИТ\nПЕРСОНАЛЬНУЮ ЦВЕТОВУЮ КАРТУ ВАШЕЙ ВНЕШНОСТИ!",
-						title_down = "ТЕПЕРЬ ВСЯ ОДЕЖДА БУДЕТ ПОДЧЕРКИВАТЬ КРАСОТУ\nВАШИХ ГЛАЗ И НЕЖНЫЙ ОТТЕНОК ВАШЕЙ КОЖИ.";
-
-					this.el = new App.managerService.slide_3.elems.menu.Scene("i/s3/3/wears.jpg", title_top, title_down);
+					this.el = new App.managerService.slide_3.elems.menu.Scene(DATA.slide_3.scene_3);
 					slide_container_3.addChild(this.el);
 					this.el.init(this.el);
+
+					var deferred = $.Deferred();
 
 					setTimeout(function() {
 						deferred.resolve();
@@ -452,13 +381,11 @@ App.managerService.slide_3 = {
 				el: null,
 
 				init: function() {
-					var deferred = $.Deferred(),
-						title_top = "ХОТИТЕ ЗНАТЬ, КАКИЕ ЦВЕТА ПОДХОДЯТ ВАМ ИДЕАЛЬНО?\nДЛЯ ВАШЕГО ИМИДЖ-ГАЙДА СПЕЦИАЛИСТ СОСТАВИТ\nПЕРСОНАЛЬНУЮ ЦВЕТОВУЮ КАРТУ ВАШЕЙ ВНЕШНОСТИ!",
-						title_down = "ТЕПЕРЬ ВСЯ ОДЕЖДА БУДЕТ ПОДЧЕРКИВАТЬ КРАСОТУ\nВАШИХ ГЛАЗ И НЕЖНЫЙ ОТТЕНОК ВАШЕЙ КОЖИ.";
-
-					this.el = new App.managerService.slide_3.elems.menu.Scene("i/s3/4/wears.jpg", title_top, title_down);
+					this.el = new App.managerService.slide_3.elems.menu.Scene(DATA.slide_3.scene_4);
 					slide_container_3.addChild(this.el);
 					this.el.init(this.el);
+
+					var deferred = $.Deferred();
 
 					setTimeout(function() {
 						deferred.resolve();
@@ -479,13 +406,11 @@ App.managerService.slide_3 = {
 				el: null,
 
 				init: function() {
-					var deferred = $.Deferred(),
-						title_top = "ХОТИТЕ ЗНАТЬ, КАКИЕ ЦВЕТА ПОДХОДЯТ ВАМ ИДЕАЛЬНО?\nДЛЯ ВАШЕГО ИМИДЖ-ГАЙДА СПЕЦИАЛИСТ СОСТАВИТ\nПЕРСОНАЛЬНУЮ ЦВЕТОВУЮ КАРТУ ВАШЕЙ ВНЕШНОСТИ!",
-						title_down = "ТЕПЕРЬ ВСЯ ОДЕЖДА БУДЕТ ПОДЧЕРКИВАТЬ КРАСОТУ\nВАШИХ ГЛАЗ И НЕЖНЫЙ ОТТЕНОК ВАШЕЙ КОЖИ.";
-
-					this.el = new App.managerService.slide_3.elems.menu.Scene("i/s3/5/wears.jpg", title_top, title_down);
+					this.el = new App.managerService.slide_3.elems.menu.Scene(DATA.slide_3.scene_5);
 					slide_container_3.addChild(this.el);
 					this.el.init(this.el);
+
+					var deferred = $.Deferred();
 
 					setTimeout(function() {
 						deferred.resolve();
@@ -506,13 +431,11 @@ App.managerService.slide_3 = {
 				el: null,
 
 				init: function() {
-					var deferred = $.Deferred(),
-						title_top = "ХОТИТЕ ЗНАТЬ, КАКИЕ ЦВЕТА ПОДХОДЯТ ВАМ ИДЕАЛЬНО?\nДЛЯ ВАШЕГО ИМИДЖ-ГАЙДА СПЕЦИАЛИСТ СОСТАВИТ\nПЕРСОНАЛЬНУЮ ЦВЕТОВУЮ КАРТУ ВАШЕЙ ВНЕШНОСТИ!",
-						title_down = "ТЕПЕРЬ ВСЯ ОДЕЖДА БУДЕТ ПОДЧЕРКИВАТЬ КРАСОТУ\nВАШИХ ГЛАЗ И НЕЖНЫЙ ОТТЕНОК ВАШЕЙ КОЖИ.";
-
-					this.el = new App.managerService.slide_3.elems.menu.Scene("i/s3/6/wears.jpg", title_top, title_down);
+					this.el = new App.managerService.slide_3.elems.menu.Scene(DATA.slide_3.scene_6);
 					slide_container_3.addChild(this.el);
 					this.el.init(this.el);
+
+					var deferred = $.Deferred();
 
 					setTimeout(function() {
 						deferred.resolve();
@@ -534,20 +457,21 @@ App.managerService.slide_3 = {
 
 				init: function() {
 
-					var style = {
-						font : "20px PlumbCondensed-Bold",
-					    fill : "#ffffff",
-					    align : "center"
-					},
-						title = new PIXI.Text("ЗАКАЗАТЬ\nИМИДЖ-ГАЙД", style);
+					var data = DATA.slide_3.orderBtn,
+						style = {
+							font 	: data.font,
+						    fill 	: data.fill,
+						    align 	: data.align
+						},
+						title = new PIXI.Text(data.text, style);
 
-					title.x = renderer.width / 2 + 50;
-					title.y = -120;
+					title.x = renderer.width / 2 + data.x;
+					title.y = data.y;
 
 					this.el = new PIXI.Graphics();
 					this.el.lineStyle(0);
-					this.el.beginFill(0xff7d7a, 1);
-					this.el.drawCircle(renderer.width / 2 + 100, -100, 60);
+					this.el.beginFill(data.circleFill, 1);
+					this.el.drawCircle(renderer.width / 2 + data.circleX, data.circleY, data.circleR);
 					this.el.endFill();
 					this.el.buttonMode = true;
 					this.el.interactive = true;
@@ -562,13 +486,15 @@ App.managerService.slide_3 = {
 					slide_container_3.addChild(this.el);
 
 					createjs.Tween.get(this.el)
-						.to({ y: renderer.height / 2 }, 1000, createjs.Ease.getPowInOut(4));
+						.to({ y: renderer.height / 2 }, data.speed, createjs.Ease.getPowInOut(4));
 
 				},
 
 				destroy: function() {
+					var data = DATA.slide_3.orderBtn;
+
 					createjs.Tween.get(this.el)
-						.to({ y: -1000 }, 1000, createjs.Ease.getPowInOut(4))
+						.to({ y: data.y }, data.speed, createjs.Ease.getPowInOut(4))
 						.call(function() {
 							this.el = null;
 						});
